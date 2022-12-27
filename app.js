654ab3f78bddd42e3bec8fb26e79f24b2a7b39b6
Diana <dianapilat232@gmail.com>
2022-12-27 18:21:21 +0200

    Homework

---
+++ app.js
@@ -0,0 +1,9 @@
+let value = prompt('Traffic light color', '');
+
+if (value == 'green') {
+ alert('Можна іти');
+}else if(value == 'yellow') {
+ alert('Приготуватись!');
+}else{
+ alert('Стоп!');
+}
